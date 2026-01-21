9bb966d94c5f9f947c21d311b68eb1f7900960df
Hiroaki Tahara <taharahiroaki@icloud.com>
2026-01-21 23:12:03 +0900

    [java] auto import, formatting, template project; [nvim] telescope 'fa' find all file;

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,8 +6,30 @@ vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = 
 
 -- tfファイルのコメント設定
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "terraform", "hcl" },
-  callback = function()
-    vim.bo.commentstring = "# %s"
-  end,
+	pattern = { "terraform", "hcl" },
+	callback = function()
+		vim.bo.commentstring = "# %s"
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.java",
+	callback = function()
+		-- 標準のLSPコマンドでインポート整理を実行
+		vim.lsp.buf.code_action({
+			context = { only = { "source.organizeImports" } },
+			apply = true,
+		})
+	end,
+})
+
+-- javaファイル専用の設定
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function()
+		vim.opt_local.expandtab = true -- タブをスペースに変換
+		vim.opt_local.tabstop = 4 -- タブをスペース4つ分として表示
+		vim.opt_local.shiftwidth = 4 -- 自動インデントの幅を4にする
+		vim.opt_local.softtabstop = 4 -- 入力時のタブ幅を4にする
+	end,
 })
